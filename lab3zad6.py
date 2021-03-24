b02fee77af751e33d89fd5f48159b15736062505
WilkAdrian1 <74956694+WilkAdrian1@users.noreply.github.com>
2021-03-24 17:47:51 +0100

    dodanie lab3

---
+++ lab3zad6.py
@@ -0,0 +1,5 @@
+import math as matma
+
+def promien(x=6,y=2,a=0,b=0):
+    return matma.sqrt((x-a)**2+(y-b)**2)
+print(promien(3,3))
---
+++ lab3zad7.py
@@ -0,0 +1,5 @@
+import math as matma
+def Pitagoras(a=6,b=8):
+    return matma.sqrt(a**2+b**2)
+
+print(Pitagoras())
